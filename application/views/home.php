9b7e13ed5f3ff7b6ec1db020efd9e8256410bc0d
Tarang Patel <tarangrockr@gmail.com>
2014-02-24 17:29:38 +0530

    Initial Commit

---
+++ application/views/home.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="StudyChem | Short description about StudyChem">
+    <meta name="author" content="Your name">
+    <title>StudyChem</title>
+    <!-- Bootstrap -->
+    <link href="<?php echo base_url('assests/css/bootstrap.min.css'); ?>" rel="stylesheet">
+    <!-- Bootstrap responsive -->
+    <link href="<?php echo base_url('assests/css/bootstrap-responsive.min.css'); ?>" rel="stylesheet">
+    <!-- Font awesome - iconic font with IE7 support --> 
+    <link href="<?php echo base_url('assests/css/font-awesome.css'); ?>" rel="stylesheet">
+    <link href="<?php echo base_url('assests/css/font-awesome-ie7.css'); ?> "rel="stylesheet">
+    <!-- Bootbusiness theme -->
+    <link href="<?php echo base_url('assests/css/boot-business.css');?>" rel="stylesheet">
+  </head>
+  <body>
+    <!-- Start: HEADER -->
+    <header>
+      <!-- Start: Navigation wrapper -->
+      <div class="navbar navbar-fixed-top">
+        <div class="navbar-inner">
+          <div class="container">
+            <a href="<?php echo site_url('home'); ?>" class="brand brand-bootbus">StudyChem</a>
+            <!-- Below button used for responsive navigation -->
+            <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <!-- Start: Primary navigation -->
+            <div class="nav-collapse collapse">        
+              <ul class="nav pull-right">
+                <li class="dropdown">
+                  <a href="<?php echo site_url('home'); ?>"> Home </a>                    
+                </li>
+                <li class="dropdown">
+                  <a href="#"> About </a>
+                  
+                </li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Contact us</a></li>
+                <li><a href="#">Sign up</a></li>
+                <li><a href="#">Sign in</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- End: Navigation wrapper -->   
+    </header>
+    <!-- End: HEADER -->
+    <!-- Start: MAIN CONTENT -->
+    <div class="content">
+    <!--  <!-- Start: slider -->
+    
+      <!-- End: PRODUCT LIST -->
+    </div>
+
+    <script type="text/javascript" src="<?php echo base_url('assests/js/jquery.min.js'); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assests/js/bootstrap.min.js'); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assests/js/boot-business.js'); ?>"></script>
+  </body>
+</html>
