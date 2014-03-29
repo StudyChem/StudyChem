0e08dbff884ac9ba1f27d11c54a34ff83f1d9dc8
Tarang Patel <tarangrockr@gmail.com>
2014-03-29 11:08:45 +0530

    minor bug in equation balancer solved

--- application/views/eqn_balancer.php
+++ application/views/eqn_balancer.php
@@ -135,5 +135,9 @@ article span.feature {
 </div>
 
 <script type="text/javascript">var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-23532931-1']); _gaq.push(['_trackPageview']); (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();</script>
+<script type="text/javascript" src="<?php echo base_url('assests/js/jquery.min.js'); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assests/js/bootstrap.min.js'); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assests/js/boot-business.js'); ?>"></script>
+
 </body>
 </html>
